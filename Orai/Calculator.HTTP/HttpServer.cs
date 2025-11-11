763c4884dbc25146b9bf9eff76f0592cdd5d3a87
Posa Levente <h376512@stud.u-szeged.hu>
2025-11-11 14:55:25 +0100

    10.het

--- Orai/Calculator.HTTP/HttpServer.cs
+++ Orai/Calculator.HTTP/HttpServer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.CompilerServices;
@@ -11,12 +13,19 @@ namespace Calculator.HTTP;
 
 public class HttpServer : IDisposable
 {
+    private readonly int _port;
     private readonly TcpListener _listener;
+    private readonly SemaphoreSlim _semaphore;
+    private readonly CancellationTokenSource _cancellationTokenSource;
     private bool _disposed;
+
 
     public HttpServer(int port)
     {
+        _port = port;
         _listener = new TcpListener(IPAddress.Any, port);
+        _semaphore = new SemaphoreSlim(10);
+        _cancellationTokenSource = new CancellationTokenSource();
     }
 
     ~HttpServer()
@@ -32,6 +41,8 @@ public class HttpServer : IDisposable
         }
 
         _listener.Dispose();
+        _semaphore.Dispose();
+        _cancellationTokenSource.Dispose();
         _disposed = true;
     }
 
@@ -41,6 +52,108 @@ public class HttpServer : IDisposable
         GC.SuppressFinalize(this);
     }
 
+    public void Start()
+    {
+        if (!_disposed)
+        {
+            _listener.Start();
+            Task.Run(ListenTask, _cancellationTokenSource.Token);
+        }
+        else
+        {
+            throw new ObjectDisposedException(nameof(HttpServer));
+        }
+    }
+
+    public void Stop()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+        _cancellationTokenSource.Cancel();
+        _listener.Stop();
+    }
+
+    private async Task ListenTask()
+    {
+        try
+        {
+            while (!_cancellationTokenSource.IsCancellationRequested) 
+            {
+                var Client = await _listener.AcceptTcpClientAsync(_cancellationTokenSource.Token);
+                await _semaphore.WaitAsync(_cancellationTokenSource.Token);
+                try
+                {
+                    await HandleClient(Client, _cancellationTokenSource.Token);
+                }
+                finally
+                {
+                    _semaphore.Release();
+                }
+
+            }
+
+        }
+        catch (OperationCanceledException ex)
+        {
+            
+        }
+    }
+
+    private async Task HandleClient(TcpClient client, CancellationToken cancellation)
+    {
+        using var stream = client.GetStream();
+
+        try
+        {
+            HttpRequest request = await HttpRequestParser.ParseAsync(stream, _port);
 
 
+
+            await SpecialHandlers.HandleNotFound(stream);
+        }
+        catch(Exception ex) 
+        {
+            Debug.WriteLine(ex.Message);
+            await SpecialHandlers.HandleServerError(stream, ex);
+        }
+        
+    }
+    internal class SpecialHandlers
+    {
+        internal static async Task HandleNotFound(NetworkStream stream) 
+        {
+            string message = "Not Found";
+            string response = $"""
+            HTTP/1.1 404 Not Found
+            Date: {DateTime.UtcNow:R}
+            Content-Type: text/plain: charset=utf-8
+            Content-Length: {Encoding.UTF8.GetByteCount(message)}
+
+            {message}
+            """;
+
+            using var writer = new StreamWriter(stream, Encoding.UTF8, leaveOpen: true);
+            await writer.WriteAsync(response);
+        }
+
+        internal static async Task HandleServerError(NetworkStream stream, Exception ex)
+        {
+            string message = "Not Found";
+            string response = $"""
+            HTTP/1.1 500 Internal Server Error
+            Date: {DateTime.UtcNow:R}
+            Content-Type: text/plain: charset=utf-8
+            Content-Length: {Encoding.UTF8.GetByteCount(message)}
+
+            {message}
+            """;
+
+            using var writer = new StreamWriter(stream, Encoding.UTF8, leaveOpen: true);
+            await writer.WriteAsync(response);
+        }
+    }
+}
+
+public interface IRequestHandler
+{
+    Task<bool> HandlerRequest(HttpRequest request, NetworkStream responseStream, CancellationToken cancellationToken);
 }
